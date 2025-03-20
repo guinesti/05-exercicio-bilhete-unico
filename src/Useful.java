794493a6677aae0aab497ad296b62295155c1810
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-20 20:58:26 -0300

    atualização da classe util com os métodos relacionados ao usuário

--- src/Useful.java
+++ src/Useful.java
@@ -1,6 +1,7 @@
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.parseInt;
 import static java.lang.Long.parseLong;
+import static java.lang.Double.parseDouble;
 
 public class Useful {
 
@@ -27,19 +28,75 @@ public class Useful {
             else {
                 switch (option) {
                     case 1:
-                        emitTicket ();
+                        emitTicket();
+                        break;
+
+                    case 2:
+                        refillTicket();
+                        break;
+
+                    case 3:
+                        checkBalance();
+                        break;
+
+                    case 4:
+                        passTheTurnstile ();
                         break;
                 }
             }
         }
+    }
 
-        public void emitTicket () {
+    public void passTheTurnstile () {
+        int position = search();
+        if (position != -1) {
+            showMessageDialog(null, ticket[position].passTheTurnstile());
+        }
+    }
+
+    public void emitTicket () {
+        if (index < ticket.length) {
             String name = showInputDialog("Nome");
             String profile = showInputDialog("Perfil --> comum ou estudante ou professor");
             long cpf = parseLong(showInputDialog("CPF"));
             User user = new User(name, profile, cpf);
-            ticket[index] = new Ticket(user);
-            index++;
+            ticket[index] = new Ticket(user); index++;
+        }
+        else {
+            showMessageDialog(null, "Bilhete não emitido");
+        }
+    }
+
+    public void checkBalance () {
+        int position = search();
+        if (position == -1) {
+            showMessageDialog(null,"CPF não encontrado");
+        }
+        else {
+            showMessageDialog(null, ticket[position].checkBalance());
+        }
+    }
+
+    public int search () {
+        int position = -1;
+        long cpf = parseLong(showInputDialog("CPF para pesquisa"));
+        for (int i = 0; i < index; i++) {
+            if (ticket[i].user.cpf == cpf) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void refillTicket () {
+        int position = search();
+        double amount;
+        if (position != -1) {
+            amount = parseDouble(showInputDialog("Valor para carregar o bilhete"));
+            ticket[position].refillTicket(amount);
+        }
+        else {
+            showMessageDialog(null,"CPF não encontrado");
         }
     }
 }
